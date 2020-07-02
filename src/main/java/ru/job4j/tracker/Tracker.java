574226bf0b9b065997c64cc61b4b05373ef2199b
IliaBnn <ilya.bnn@gmail.com>
2020-07-02 17:13:10 +0400

    5. Tracker - хранилище [#313429]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,29 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] itemsWithoutNull = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (items[i] != null) {
+                itemsWithoutNull[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsWithoutNull = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < this.items.length; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsWithoutNull[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(itemsWithoutNull, size);
     }
 }
